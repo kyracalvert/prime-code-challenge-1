9aa1a42af5a9ed2da9c2b0ac577bf85b03f0a40f
Kyra Crawford Calvert <kyracalvert@gmail.com>
2018-07-30 14:21:39 -0500

    Generate button created.

---
+++ scripts/client.js
@@ -0,0 +1,28 @@
+$(document).ready(readyNow);
+
+function readyNow(){
+ console.log('jQuery ready');
+ $('generate-button').on('click', generate);
+ $('swap-button').on('click', swap);
+ $('delete-button').on('click', remove);
+}
+
+let buttonCounter = 0;
+
+function generate() {
+buttonCounter ++;
+$('body').append('<div class="buttonHolder"></div>');
+$('#button').empty('<p> Button Clicked:' +buttonCounter+ '</p>');
+$('#button').append('<p> Button Clicked:' +buttonCounter+ '</p>');
+console.log('button clicked:', buttonCounter);
+
+}
+
+function swap() {
+
+}
+
+function remove() {
+    console.log('remove');
+    $(this).parent().parent().remove();
+}
