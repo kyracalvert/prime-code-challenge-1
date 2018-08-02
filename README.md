7acead025911a6ea94ce6558862b1ce21ea8cae0
Kyra Crawford Calvert <kyracalvert@gmail.com>
2018-08-02 09:01:41 -0500

    Complete and functional.

--- README.md
+++ README.md
@@ -6,12 +6,12 @@
 
 [x] Inside the <div> element you created in the previous step, create a <p> element that shows how many times you have clicked the button from the first step.
 
-[ ] Inside the <div> element you created in step two, append two <button> elements. The text of the two buttons should read "Swap" and "Delete".
+[x] Inside the <div> element you created in step two, append two <button> elements. The text of the two buttons should read "Swap" and "Delete".
 
-[ ] In a CSS file, any <div> created using the "Generate" button should have a Red background-color.
+[x] In a CSS file, any <div> created using the "Generate" button should have a Red background-color.
 
-[ ] Clicking a "Swap" <button> should change its parent background-color from Red to Yellow.
+[x] Clicking a "Swap" <button> should change its parent background-color from Red to Yellow.
 
-[ ] Clicking the "Swap" <button> again should turn the color back to Red.
+[x] Clicking the "Swap" <button> again should turn the color back to Red.
 
-[ ] Clicking a "Delete" <button> should remove its parent <div> container (and all of its contents).
+[x] Clicking a "Delete" <button> should remove its parent <div> container (and all of its contents).
--- scripts/client.js
+++ scripts/client.js
@@ -1,21 +1,40 @@
 $(document).ready(readyNow);
 
-function readyNow(){
- console.log('jQuery ready');
- $('generate-button').on('click', generate);
- $('swap-button').on('click', swap);
- $('delete-button').on('click', remove);
+function readyNow() {
+    console.log('jQuery ready');
+    $('#generate-button').on('click', generate);
+    // Hey, jQuery, there is an element on page load called "buttonClicked". Listen for clicks there!
+    // If the click happens on something with the class of "swap", run the swap function. 
+    // Love, Kyra.
+    $('.buttonClicked').on('click', '.swapButton', swap);
+    $('.buttonClicked').on('click', '.deleteButton', deleteButton);
 }
 
 let buttonCounter = 0;
 
 //this was counting the button clicks on 'generate', but has stopped working. 
 function generate() {
-buttonCounter ++;
-$('body').append('<div class="buttonHolder"></div>');
-$('#button').empty('<p> Button Clicked:' +buttonCounter+ '</p>');
-$('#button').append('<p> Button Clicked:' +buttonCounter+ '</p>');
-console.log('button clicked:', buttonCounter);
+    buttonCounter++;
+    let newDiv = '<div class ="newDiv"><p>' + buttonCounter + '</p><button class="swapButton">Swap</button> <button class="deleteButton">Delete</button></div>';
+    //append a dive when the button in clicked. 
+    console.log('button clicked!');
+    $('.buttonClicked').append(newDiv);
+
+
+    // buttonCounter ++;
+    // $('body').append('<div class="buttonHolder"></div>');
+    // $('#button').empty('<p> Button Clicked:' +buttonCounter+ '</p>');
+    // $('#button').append('<p> Button Clicked:' +buttonCounter+ '</p>');
+    // console.log('button clicked:', buttonCounter);
 
 }
 
+function swap() {
+    console.log ('swap clicked!');
+    $(this).parent().toggleClass('yellow');
+}
+
+function deleteButton() {
+console.log('delete button clicked');
+$(this).parent().remove();
+}
